1245f59b16538d1428b58a0bfa8c0ec504d2f6b4
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-12-03 17:47:21 +0600

    Update

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.send("Running");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("App Run");
+});
