a798132e5b10f78a8dcdd06c3f97ccaba0fa2846
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-12-03 19:49:07 +0600

    a

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ app.get("/", (req, res) => {
   res.send("Running");
 });
 
-app.post("/name", (req, res) => {});
+app.post("/name", (req, res) => {
+  if (req.body.name) {
+    return res.json({ name: req.body.name });
+  } else {
+    res.status(400).json({ error: "No Name Provide" });
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log("App Run");
