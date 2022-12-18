1b0d196ff4b8744e5b52b4726d71ca3432b9cc2f
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-12-18 18:00:38 +0600

    User Data send To Database

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 // @route Get /api/auth/test
 // @desc Test the auth route
@@ -11,5 +13,27 @@ router.get("/test", (req, res) => {
 // @route Post /api/auth/register
 // @desc Create a New User
 // @access Public
+
+router.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    // create new user
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //Save user
+    const savedUser = await newUser.save();
+    // return new user
+    return res.json(savedUser);
+  } catch (err) {
+    // error here
+    console.log(err);
+
+    res.status(500).send(err.message);
+  }
+});
 
 module.exports = router;
